68d191e98cd7b72e13d8772bd3958fb0846d4977
Ahmet <ahmet.atay@hotmail.com>
2015-02-02 00:46:16 +0200

    Active Development

--- src/Glad/GladModelInterface.php
+++ src/Glad/GladModelInterface.php
@@ -4,5 +4,6 @@ namespace Glad;
 
 interface GladModelInterface {
 	public static function newUser(array $credentials);
-	//public static function find();
+	public static function getIdentity($identity);
+	public static function getIdentityWithId($userId);
 }
